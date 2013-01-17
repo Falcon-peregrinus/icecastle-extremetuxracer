6d82c2bb5aad14ddcc822c7c5286de0247959271
Falcon-peregrinus <Falcon-peregrinus@server.fake>
2013-01-17 22:46:27 +0700

    Enough for today

--- icecastle/course.tcl
+++ icecastle/course.tcl
@@ -2,9 +2,9 @@
 
 tux_course_name "Icecastle"
 
-tux_course_dim 100 800 60 785
-tux_start_pt 38 3.5
-tux_angle 25
+tux_course_dim 100 800 100 785
+tux_start_pt 45 8
+tux_angle 30
 
 tux_elev_scale 22
 tux_elev elev.png
